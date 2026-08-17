32bbd035c1823947033849eb0e7c2bb25c8d37e7
LeoR <leorsky77@gmail.com>
2026-08-17 22:09:15 +0300

    Add get_forecast method

--- src/project_new/Class/WeatherClient.py
+++ src/project_new/Class/WeatherClient.py
@@ -20,3 +20,19 @@ class WeatherClient:
         wind_direction = data['current']['wind_direction_10m']
         weather_code = data['current']['weather_code']
         return temperature, wind_speed, wind_direction, weather_code
+
+
+    def get_forecast(self, latitude, longitude, days=3):
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "daily": "temperature_2m_max,temperature_2m_min,weather_code",
+            "forecast_days": days
+        }
+
+        data = self.api_client.get(f'/v1/forecast', params=params)
+        temperature_max = data['daily']['temperature_2m_max']
+        temperature_min = data['daily']['temperature_2m_min']
+        weather_code = data['daily']['weather_code']
+        time_d = data['daily']['time']
+        return temperature_max, temperature_min, weather_code, time_d
