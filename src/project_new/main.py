ed37500c209e3e360cd64b46597ac0eac2c9866f
LeoR <leorsky77@gmail.com>
2026-08-17 22:12:30 +0300

    Import all classes into main

---
+++ src/project_new/main.py
@@ -0,0 +1,4 @@
+from project_new.src.project_new.Class.APIClient import APIClient
+from project_new.src.project_new.Class.WeatherClient import WeatherClient
+from project_new.src.project_new.Class.Weather import WeatherDays
+from project_new.src.project_new.Class.Weather import Weather
