5ed262133e279cbcc2ca5d0dd1e0c6b5a33c76f8
LeoR <leorsky77@gmail.com>
2026-08-17 22:15:56 +0300

    Create APIClient instance

--- src/project_new/main.py
+++ src/project_new/main.py
@@ -2,3 +2,6 @@ from project_new.src.project_new.Class.APIClient import APIClient
 from project_new.src.project_new.Class.WeatherClient import WeatherClient
 from project_new.src.project_new.Class.Weather import WeatherDays
 from project_new.src.project_new.Class.Weather import Weather
+
+
+api_client = APIClient('https://api.open-meteo.com')
