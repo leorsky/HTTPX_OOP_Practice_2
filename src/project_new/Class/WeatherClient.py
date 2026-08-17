1c0fe46b57d08733150c01cfdbf703a072bd4b51
LeoR <leorsky77@gmail.com>
2026-08-17 22:08:12 +0300

    Add get_current_weather method

--- src/project_new/Class/WeatherClient.py
+++ src/project_new/Class/WeatherClient.py
@@ -1,3 +1,22 @@
 class WeatherClient:
     def __init__(self, api_client):
         self.api_client = api_client
+
+    def get_current_weather(self, latitude, longitude):
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "current": [
+                "temperature_2m",
+                "wind_speed_10m",
+                "wind_direction_10m",
+                "weather_code"
+            ]
+        }
+
+        data = self.api_client.get(f'/v1/forecast', params=params)
+        temperature = data['current']['temperature_2m']
+        wind_speed = data['current']['wind_speed_10m']
+        wind_direction = data['current']['wind_direction_10m']
+        weather_code = data['current']['weather_code']
+        return temperature, wind_speed, wind_direction, weather_code
