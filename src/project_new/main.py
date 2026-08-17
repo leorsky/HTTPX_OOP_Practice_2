1cd45d77d051be48a3b7db5c1a73fe23f2caa47c
LeoR <leorsky77@gmail.com>
2026-08-17 22:18:41 +0300

    Add attributes for city selection and forecast duration

--- src/project_new/main.py
+++ src/project_new/main.py
@@ -7,3 +7,7 @@ from project_new.src.project_new.Class.Weather import Weather
 api_client = APIClient('https://api.open-meteo.com')
 
 weather_client = WeatherClient(api_client)
+
+latitude = input('Enter Latitude: ')
+longitude = input('Enter Longitude: ')
+days = input('Enter Days: ')
