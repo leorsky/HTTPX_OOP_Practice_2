a8278246b1da9af8fef66e68b1ffa3a2f4d731d7
LeoR <leorsky77@gmail.com>
2026-08-17 22:03:20 +0300

    Add GET method

--- src/project_new/Class/APIClient.py
+++ src/project_new/Class/APIClient.py
@@ -3,3 +3,8 @@ import httpx
 class APIClient:
     def __init__(self, base_url):
         self.client = httpx.Client(base_url=base_url)
+
+    def get(self, endpoint='', params=None):
+        response = self.client.get(endpoint, params=params)
+        response.raise_for_status()
+        return response.json()
