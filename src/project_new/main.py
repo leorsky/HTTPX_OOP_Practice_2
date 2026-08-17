5ce473e6f0ef027459110de9ea3c592b40111bc3
LeoR <leorsky77@gmail.com>
2026-08-17 22:19:41 +0300

    Implement classes and display one-day weather information

--- src/project_new/main.py
+++ src/project_new/main.py
@@ -11,3 +11,14 @@ weather_client = WeatherClient(api_client)
 latitude = input('Enter Latitude: ')
 longitude = input('Enter Longitude: ')
 days = input('Enter Days: ')
+
+if days == '1':
+    temperature, wind_speed, wind_direction, weather_code = weather_client.get_current_weather(latitude, longitude)
+    weather_1 = Weather(temperature, wind_speed, wind_direction, weather_code)
+
+    print(f'\nCurrent weather\n'
+          f'----------------\n'
+          f'Temperature: {weather_1.temperature} °C\n'
+          f'Wind speed: {weather_1.wind_speed} km/h\n'
+          f'Wind direction: {weather_1.wind_direction}°\n'
+          f'Weather code: {weather_1.weather_code}\n')
